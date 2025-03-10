f9974ae2aa478678dfc866a6e0b84b8f956e6b40
Ishan R <rathiishan0@gmail.com>
2025-03-09 23:05:46 -0400

    Redirecting to port 0.0.0.0 for prod

--- app.py
+++ app.py
@@ -182,6 +182,5 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-    #app.run(host="0.0.0.0", port=port, debug=True)
-    app.run()
+    app.run(host="0.0.0.0", port=port, debug=True)
 
